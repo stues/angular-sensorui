d79c8af12b596ddc2c4b4416445b0bd7c55f3ef0
stue <stue@ntbstue>
2014-09-13 15:38:31 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sensorwebuiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sensorwebuiApp
+ */
+angular.module('sensorwebuiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
