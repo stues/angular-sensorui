06b7560b8c1c110dcb85e355cd72315371cffedd
stue <stue@ntbstue>
2014-12-19 16:01:16 +0100

    allow loading sos deltas shorter than 1s

--- app/scripts/sos-service.js
+++ app/scripts/sos-service.js
@@ -18,29 +18,59 @@ angular.module('angularol3jsuiApp')
      * @returns {*}
      */
     function getNewEntries(dateFrom, dateTo) {
-      var request = $http({
-        method: "post",
-        url: sosConfig.url,
-        data: {
-          "request": "GetObservation",
-          "service": "SOS",
-          "version": "2.0.0",
-          "procedure": sosConfig.procedure,
-          "offering": sosConfig.offering,
-          "temporalFilter": [
-            {
-              "during": {
-                "ref": "om:phenomenonTime",
-                "value": [
-                  getShortISOString(dateFrom),
-                  getShortISOString(dateTo)
-                ]
+      var request;
+      if(sosConfig.requestType == "application/xml"){
+        request = $http({
+          method: "post",
+          url: sosConfig.poxURL,
+          data: '<?xml version="1.0" encoding="UTF-8"?>'
+          +'<sos:GetObservation service="SOS" version="2.0.0"'
+          +'  xmlns:sos="http://www.opengis.net/sos/2.0"'
+          +'  xmlns:fes="http://www.opengis.net/fes/2.0"'
+          +'  xmlns:gml="http://www.opengis.net/gml/3.2"'
+          +'  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/sos/2.0 http://schemas.opengis.net/sos/2.0/sos.xsd">'
+          +'	<sos:procedure>' + sosConfig.procedure + '</sos:procedure>'
+          +'	<sos:offering>' + sosConfig.offering + '</sos:offering>'
+          +'	<sos:temporalFilter>'
+          +'		<fes:During>'
+          +'		<fes:ValueReference>phenomenonTime</fes:ValueReference>'
+          +'			<gml:TimePeriod gml:id="t1">'
+          +'				<gml:beginPosition>' + dateFrom.toISOString() + '</gml:beginPosition>'
+          +'				<gml:endPosition>' + dateTo.toISOString() + '</gml:endPosition>'
+          +'			</gml:TimePeriod>'
+          +'		</fes:During>'
+          +'	</sos:temporalFilter>'
+          +'  <sos:responseFormat>application/json</sos:responseFormat>'
+          +'</sos:GetObservation>'
+        });
+      }
+      else if(sosConfig.requestType == "application/json"){
+         request = $http({
+          method: "post",
+          url: sosConfig.jsonURL,
+          data: {
+            "request": "GetObservation",
+            "service": "SOS",
+            "version": "2.0.0",
+            "procedure": sosConfig.procedure,
+            "offering": sosConfig.offering,
+            "temporalFilter": [
+              {
+                "during": {
+                  "ref": "om:phenomenonTime",
+                  "value": [
+                    getShortISOString(dateFrom),
+                    getShortISOString(dateTo)
+                  ]
+                }
               }
-            }
-          ]
-        }
-      });
-
+            ]
+          }
+        });
+      }
+      else {
+        throw "Unknown Request Type";
+      }
       return (request.then(handleSuccess, handleError));
     }
 
