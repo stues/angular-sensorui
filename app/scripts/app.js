99ffcf04bce70db1d25e766d6fdf759c92fe5625
stue <stue@ntbstue>
2015-06-19 15:45:53 +0200

    Changed Home page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/#', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
       .when('', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
